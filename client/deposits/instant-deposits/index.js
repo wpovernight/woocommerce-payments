040b6c8fbdc1bc39a767a9aa0c26619bbbdb0211
Jesse Pearson <jesse.pearson@automattic.com>
2021-03-03 12:53:01 -0400

    Swapped notices in modal to snackbars on page.

--- client/deposits/instant-deposits/index.js
+++ client/deposits/instant-deposits/index.js
@@ -4,29 +4,30 @@
  * External dependencies
  */
 import { Button } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
+import { useDispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 import InstantDepositModal from './modal';
+import Currency from "@woocommerce/currency";
+// TODO: Use the proper WCPay currency.
+const currency = new Currency();
 
-const InstantDepositButton = ( {
-	// eslint-disable-next-line camelcase
-	balance: { amount, fee, net, transaction_ids },
-} ) => {
+// eslint-disable-next-line camelcase
+const InstantDepositButton = ( { balance: { amount, fee, net, transaction_ids } } ) => {
 	const [ isModalOpen, setModalOpen ] = useState( false );
 	const [ inProgress, setInProgress ] = useState( false );
-	const [ notice, setNotice ] = useState( null );
+	const { createSuccessNotice, createErrorNotice } = useDispatch('core/notices' );
 
 	// TODO: Use wp.data
 	const submit = async () => {
 		try {
 			setInProgress( true );
-			setNotice( false );
 			await apiFetch( {
 				path: '/wc/v3/payments/deposits',
 				method: 'POST',
@@ -36,22 +37,30 @@ const InstantDepositButton = ( {
 					transaction_ids,
 				},
 			} );
-			// TODO: Success notice? Full-reload the page so the new deposit appears?
-			setNotice( { result: 'success' } );
+			// TODO: Full-reload the page so the new deposit appears?
+			createSuccessNotice(
+				sprintf(
+					/* translators: %s: Monetary amount to deposit */
+					__(
+					'Instant deposit for %1$s in transit.', 'woocommerce-payments'),
+					currency.formatCurrency( net / 100 )
+				)
+			);
 		} catch ( err ) {
-			setNotice( { result: 'error', code: err.code, message: err.message } );
+			createErrorNotice(
+				__(
+					'The deposit was not successful.',
+					'woocommerce-payments'
+				)
+			);
 		} finally {
 			setInProgress( false );
+			setModalOpen( false );
 		}
 	};
 
 	const onClose = () => {
-		// If it has a notice, we want to refresh the page.
-		if ( notice ) {
-			document.location.reload();
-		} else {
-			setModalOpen( false );
-		}
+		setModalOpen( false );
 	}
 
 	return (
@@ -67,7 +76,6 @@ const InstantDepositButton = ( {
 					inProgress={ inProgress }
 					onSubmit={ submit }
 					onClose={ onClose }
-					notice={ notice }
 				/>
 			) }
 		</>

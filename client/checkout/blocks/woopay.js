fdfeee42ffa374df5d14607fb7d3fefd1c84c694
Paul Dechov <paul.dechov@automattic.com>
2021-08-19 01:13:58 -0400

    Don't pass items – the cart session is already available

--- client/checkout/blocks/woopay.js
+++ client/checkout/blocks/woopay.js
@@ -1,21 +1,10 @@
 /**
- * External dependencies
- */
-import { useSelect } from '@wordpress/data';
-
-/**
  * Internal dependencies
  */
 import { getConfig } from 'utils/checkout';
 import woopayButtonSrc from 'wcpay/../assets/images/buy-now-woopay.png';
 
 const WooPay = ( { isStatic } ) => {
-	const fullItems = useSelect(
-		( select ) => select( 'wc/store/cart' ).getCartData().items || []
-	);
-	const items = fullItems.map( ( { id, quantity } ) => ( { id, quantity } ) );
-	const encodedItems = encodeURIComponent( JSON.stringify( items ) );
-
 	const image = <img src={ woopayButtonSrc } alt="Buy now with WooPay" />;
 	if ( isStatic ) {
 		return image;
@@ -23,7 +12,7 @@ const WooPay = ( { isStatic } ) => {
 
 	const encodedOrigin = encodeURIComponent( window.location.origin );
 	const woopayUrl = 'http://localhost:8086/woopay/';
-	const href = `${ woopayUrl }?items=${ encodedItems }&origin=${ encodedOrigin }`;
+	const href = `${ woopayUrl }?origin=${ encodedOrigin }`;
 	return <a href={ href }>{ image }</a>;
 };
 

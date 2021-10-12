e32323b4c570ba2e12c4987172e586cefb653db7
James Allan <james.allan@automattic.com>
2021-10-12 13:48:17 +1000

    Add the wcpay subscriptions feature flag to the to_array() function

--- includes/class-wc-payments-features.php
+++ includes/class-wc-payments-features.php
@@ -110,6 +110,7 @@ class WC_Payments_Features {
 				'upeSettingsPreview'      => self::is_upe_settings_preview_enabled(),
 				'multiCurrency'           => self::is_customer_multi_currency_enabled(),
 				'accountOverviewTaskList' => self::is_account_overview_task_list_enabled(),
+				'subscriptions'           => self::is_wcpay_subscriptions_enabled(),
 			]
 		);
 	}
